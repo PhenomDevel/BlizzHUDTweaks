7779eb34aaef539af9eaec411a707e522aefa432
Kevin <13941819+PhenomDevel@users.noreply.github.com>
2022-11-26 12:46:48 +0100

    [fix] [#9] * Use defaultConfig frameNames for copyfrom list instead

--- BlizzHUDTweaks/BlizzHUDTweaks.lua
+++ BlizzHUDTweaks/BlizzHUDTweaks.lua
@@ -397,7 +397,7 @@ local frameTable
 function addon:GetFrameTable()
   if not frameTable then
     frameTable = {}
-    for frameName, _ in pairs(frameMapping) do
+    for frameName, _ in pairs(defaultConfig.profile) do
       local frameOptions = self.db.profile[frameName]
       if type(frameOptions) == "table" then
         if not frameOptions.Hidden then
@@ -405,7 +405,6 @@ function addon:GetFrameTable()
         end
       end
     end
-    frameTable["*Global*"] = self.db.profile["*Global*"].displayName
   end
 
   return frameTable
