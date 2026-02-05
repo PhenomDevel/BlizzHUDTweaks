3a85ba0dec0443dcbdaf7f305b1ff3f79e93f092
Kevin <13941819+PhenomDevel@users.noreply.github.com>
2026-02-05 19:15:20 +0100

    cleanup code for tooltip spell id

--- BlizzHUDTweaks/Tooltip.lua
+++ BlizzHUDTweaks/Tooltip.lua
@@ -2,15 +2,6 @@ local addon = LibStub("AceAddon-3.0"):GetAddon("BlizzHUDTweaks")
 local Tooltip = addon:GetModule("Tooltip")
 
 local Original_GameTooltip_SetDefaultAnchor = GameTooltip_SetDefaultAnchor
-Tooltip.spellIdHookInstalled = false
-
-local function addSpellID(tooltip, spellID)
-    if not spellID then return end
-
-    tooltip:AddLine(" ")
-    tooltip:AddLine("|cff00ff00Spell ID:|r " .. spellID)
-    tooltip:Show()
-end
 
 local function anchorTooltipToMouse()
     GameTooltip_SetDefaultAnchor = function(tooltip, parent)
@@ -18,41 +9,9 @@ local function anchorTooltipToMouse()
     end
 end
 
-local function showSpellID()
-    if not Tooltip.spellIdHookInstalled then
-        hooksecurefunc(GameTooltip, "SetSpellByID", function(self, spellID)
-            addSpellID(self, spellID)
-        end)
-
-        hooksecurefunc(GameTooltip, "SetSpellBookItem", function(self, slot, bookType)
-            local spellBookItemInfo = C_SpellBook.GetSpellBookItemInfo(slot, bookType)
-            if spellBookItemInfo and spellBookItemInfo.spellID then
-                addSpellID(self, spellBookItemInfo.spellID)
-            end
-        end)
-    end
-
-    Tooltip.spellIdHookInstalled = true
-end
-
 
 -------------------------------------------------------------------------------
 -- Public API
-
-function Tooltip:ShowSpellID()
-    if Tooltip:IsEnabled() then
-        local profile = addon:GetProfileDB()
-        if profile["TooltipShowSpellID"] then
-            showSpellID()
-        end
-    end
-end
-
-function Tooltip:HideSpellID()
-    if Tooltip:IsEnabled() then
-        addon:Print("You have to /reload for this option to take effect.", addon:ColoredString("Tooltip", "fcba03"))
-    end
-end
 
 function Tooltip:AnchorTooltipToMouse()
     if Tooltip:IsEnabled() then
@@ -72,7 +31,6 @@ end
 function Tooltip:InstallHooks()
     if Tooltip:IsEnabled() then
         Tooltip:AnchorTooltipToMouse()
-        Tooltip:ShowSpellID()
     end
 end
 
